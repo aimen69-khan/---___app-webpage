7ac23ba2c6d45053a39f49d02765c7901a6c7e57
Ahmed Raza <aimen69khan@gmail.com>
2026-06-01 09:25:47 +0500

    Done

--- app.js
+++ app.js
@@ -10,11 +10,12 @@
  function divided(a,b){
     return a / b;
 }
+var userInput_67 = 12345678910;
 
 function mainOnClick(){
 let userInput_1 = parseFloat(document.getElementById('firstInput').value || 0);
 let userInput_2 = parseFloat(document.getElementById('secondInput').value || 0);
-let userInput_3 = document.getElementById('opreator').value || 0;
+let userInput_3 = document.getElementById('opreator').value || 0 .trim();
 let userInput_4 = document.getElementById('result');
 let finalResult = 0;
 
@@ -36,3 +37,4 @@ if(userInput_3 === "+"){
     }
   userInput_4.textContent = "Result: " + finalResult;
 }
+
